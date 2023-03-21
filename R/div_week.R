7c1cd8594ec641dc2902703ccedf9c39e125f5cd
MichellS3 <salcanshirley1b@gmail.com>
2023-03-21 01:11:46 -0500

    Aumento de funciones

---
+++ R/div_week.R
@@ -0,0 +1,16 @@
+#' Principal measures of disease cases by epidemiological week
+#'
+#' @param SEe Epidemiological week
+#'
+#' @return Total cases in this epidemiological week
+#' @import dplyr
+#'
+#' @examples
+#' For this is necessary a data frame with an specifically disease and year.
+#' div_week(54)
+
+div_week<-function(SEe){
+  Table01 <- Tablex %>% filter (SE==SEe)
+  cases_acum<-sum(Table01$Casos)
+  cases_acum
+}
---
+++ R/div_year.R
@@ -0,0 +1,17 @@
+#' Amount instance of disease by year
+#'
+#' @param x vector of length two (illness or disease, year)
+#'
+#' @return data frame with total cases by year
+#' @import dplyr
+#'
+#' @examples
+#' div_year("Malaria",2016)
+
+div_year<-function(x){
+  name<-x[1]
+  year<-x[2]
+  Table01<- Table %>% filter(Año==year, EVENTOS==name)
+  cases_acum<-sum(Table01$Casos)
+  cases_acum
+}
